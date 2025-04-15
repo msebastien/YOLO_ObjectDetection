4e8924bb78ec1d134ed75c8505b560413d531138
Sébastien Maes <sebastien.maes@protonmail.com>
2025-04-15 23:29:56 +0200

    Add MediaResource class

---
+++ media_resource.py
@@ -0,0 +1,82 @@
+from enum import Enum
+from typing import Tuple, Union
+import cv2
+
+
+class MediaResourceType(Enum):
+    IMAGE = 0
+    STREAM = 1
+
+
+class MediaResource(object):
+    def __init__(
+        self,
+        resource_location: Union[str, int],
+        type: MediaResourceType,
+    ):
+        self._resource_location = resource_location
+        self._type = type
+        self._capture_api = cv2.CAP_ANY
+        self._is_camera = isinstance(resource_location, int)
+
+        # If a camera ID is specified
+        if self._is_camera:
+            self._type = MediaResourceType.STREAM
+            self._capture_api = cv2.CAP_V4L2
+
+        if type == MediaResourceType.STREAM:
+            self._res = cv2.VideoCapture(self._resource_location, self._capture_api)
+        else:
+            self._res = cv2.imread(self._resource_location, cv2.COLOR_RGB2BGR)
+
+    def acquire(self) -> Tuple[bool, cv2.typing.MatLike]:
+        ret = False
+        frame = None
+
+        if self._type == MediaResourceType.STREAM:
+            ret, frame = self._res.read()
+        else:
+            ret = True
+            frame = self._res
+
+        return ret, frame
+
+    def is_opened(self) -> bool:
+        ret = False
+
+        if type == MediaResourceType.STREAM:
+            ret = self._res.isOpened()
+
+        return ret
+
+    def type(self) -> MediaResourceType:
+        return self._type
+
+    def fps(self) -> float:
+        freq = 0.0
+
+        if type == MediaResourceType.STREAM:
+            freq = self._res.get(cv2.CAP_PROP_FPS)
+
+        return freq
+
+    def path(self) -> str:
+        path = ""
+
+        if self._is_camera:
+            path = "/dev/video" + self._resource_location
+        else:
+            path = self._resource_location
+
+        return path
+
+    def frame_size(self):
+        width = height = 0
+        if type == MediaResourceType.STREAM:
+            width = int(self._res.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(self._res.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        else:
+            width = self._res.shape[1]
+            height = self._res.shape[0]
+
+        return width, height
