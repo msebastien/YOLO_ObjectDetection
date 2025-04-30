970d9dedc15e29589434c47a7606ea58e24d75e4
Sébastien Maes <sebastien.maes@protonmail.com>
2025-04-30 23:51:34 +0200

    utils.py: add 2 methods for writing output image

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import os
+import random
+import string
 import tempfile
+import cv2
 
 
 def copy_video_to_temp_file(file_path):
@@ -9,3 +12,17 @@ def copy_video_to_temp_file(file_path):
     with open(video_path, "wb") as f:
         with open(file_path, "rb") as g:
             f.write(g.read())
+
+
+def _get_image_path():
+    id = "".join(
+        random.choices(
+            string.ascii_uppercase + string.ascii_lowercase,
+            k=5,
+        )
+    )
+    return f"captures/annotated_output_{id}.jpg"
+
+
+def _write_image(frame, path):
+    cv2.imwrite(path, frame)
