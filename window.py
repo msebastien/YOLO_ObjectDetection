d408c9743941a1b7d20b05d89489e85192169082
Sébastien Maes <sebastien.maes@protonmail.com>
2025-05-01 20:20:55 +0200

    window.py: when created, fill window with black color

--- window.py
+++ window.py
@@ -5,17 +5,23 @@ import cv2
 
 class Window(object):
     def __init__(self, width, height, title="Video"):
+        self._width, self._height = width, height
+
+        # Init SDL, Font, Window
         sdl2.ext.init()
-        self.window = sdl2.ext.Window(title, size=(width, height))
-        self.window.show()
-        self.width, self.height = width, height
+        self._window = sdl2.ext.Window(title, size=(width, height))
+        self._window.show()
+        # self._renderer = sdl2.ext.Renderer(self._window)
+
+        # Init default window content
+        self._fill_color(0, 0, 0)
 
     def paint(self, img):
-        img = cv2.resize(img, (self.width, self.height))
+        img = cv2.resize(img, (self._width, self._height))
 
         # Retrieves a 3D numpy array that represents the pixel data of
         # the window's surface
-        surface = sdl2.ext.pixels3d(self.window.get_surface())
+        surface = sdl2.ext.pixels3d(self._window.get_surface())
 
         # Updates the pixel data of the window's surface with the resized image
         # img.swapaxes(0, 1) swaps the axes of the image array
@@ -23,9 +29,7 @@ class Window(object):
         surface[:, :, 0:3] = img.swapaxes(0, 1)
 
         # Refreshes the window to display the updated surface
-        self.window.refresh()
-
-        return
+        self._window.refresh()
 
     def close_requested(self):
         ret = False
@@ -41,4 +45,8 @@ class Window(object):
         return ret
 
     def close(self):
-        self.window.close()
+        self._window.close()
+
+    def _fill_color(self, r=255, g=255, b=255, a=255):
+        sdl2.ext.draw.fill(self._window.get_surface(), sdl2.ext.Color(r, g, b, a))
+        self._window.refresh()
