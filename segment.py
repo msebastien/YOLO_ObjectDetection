8060af6938a0a4f3afabba2846ee574d698faf46
Sébastien Maes <sebastien.maes@protonmail.com>
2025-05-28 11:06:41 +0200

    Add Pose and Segment tasks

---
+++ segment.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from typing import Union
+import logging
+import cv2
+from ultralytics import YOLO
+from ultralytics.engine.results import Results
+
+# import supervision as sv
+
+from task import Task
+
+logger = logging.getLogger(__name__)
+
+
+class Segment(Task):
+    def __init__(
+        self,
+        model: Union[str, Path] = "yolo11s-seg.pt",
+        confidence_threshold: float = 0.25,
+    ) -> None:
+        self.model_path = model if model else "yolo11s-seg.pt"
+        self._model = YOLO(model=self.model_path, task="segment")
+        self._conf_threshold = confidence_threshold
+
+    def train(self, dataset, epochs, device="cpu", resume=False):
+        pass
+
+    def predict(self, frame):
+        pass
+
+    def _annotate(
+        self,
+        results: Results,
+        original_img: cv2.typing.MatLike,
+    ) -> cv2.typing.MatLike:
+        pass
