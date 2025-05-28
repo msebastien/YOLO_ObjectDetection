7ba069b445da9cd27fb249c4fef655e10c03016d
Sébastien Maes <sebastien.maes@protonmail.com>
2025-05-28 11:06:12 +0200

    Inference class modified to Detection class and inherit Task base class

--- detection.py
+++ detection.py
@@ -6,23 +6,30 @@ from ultralytics import YOLO
 from ultralytics.engine.results import Results
 import supervision as sv
 
+from task import Task
+
 logger = logging.getLogger(__name__)
 
 
-class Inference(object):
+class Detection(Task):
     def __init__(
         self,
-        model: Union[str, Path] = None,
+        model: Union[str, Path] = "yolov12s.pt",
         confidence_threshold: float = 0.25,
     ) -> None:
         self.model_path = model if model else "models/detect/yolov12s_handgestures.pt"
-        self._model = YOLO(self.model_path)
+        self._model = YOLO(model=self.model_path, task="detect").va
         self._conf_threshold = confidence_threshold
 
-    def predict(
-        self,
-        frame: cv2.typing.MatLike,
-    ) -> Union[cv2.typing.MatLike, None]:
+    def train(self, dataset, epochs, device="cpu", resume=False):
+        return self._model.train(
+            data=dataset,
+            epochs=epochs,
+            workers=8,
+            device=device,
+        )
+
+    def predict(self, frame):
         if frame is not None and frame.any():
             width, height = (frame.shape[1], frame.shape[0])
             results = self._model.predict(
@@ -32,6 +39,9 @@ class Inference(object):
             )
             return self._annotate(results, frame)
         return None
+
+    def val(self, dataset, device, verbose):
+        pass
 
     def _annotate(
         self,
