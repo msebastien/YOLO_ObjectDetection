f9750d4ca35e2425cc655e265dd23809794de358
Sébastien Maes <sebastien.maes@protonmail.com>
2025-07-12 21:13:13 +0200

    segment.py: implement train method

--- segment.py
+++ segment.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Union
+import datetime
 import logging
 import cv2
 from ultralytics import YOLO
@@ -22,8 +23,40 @@ class Segment(Task):
         self._model = YOLO(model=self.model_path, task="segment")
         self._conf_threshold = confidence_threshold
 
-    def train(self, dataset, epochs, device="cpu", resume=False):
-        pass
+    def train(
+        self,
+        dataset="coco.yaml",
+        img_size=640,
+        epochs=100,
+        device="cpu",
+        plots=False,
+    ):
+        date = datetime.datetime.now()
+        name = date.strftime(f"{self._model.model_name}_segment_%Y-%m-%d_%H-%M-%S")
+        return self._model.train(
+            data=dataset,
+            imgsz=img_size,
+            multi_scale=False,
+            epochs=epochs,
+            patience=100,
+            batch=16,
+            fraction=1.0,
+            save=True,
+            save_period=-1,
+            resume=False,
+            amp=True,
+            cache=True,
+            workers=8,
+            device=device,
+            project="training",
+            name=name,
+            overlap_mask=True,
+            mask_ratio=4,
+            dropout=0.0,
+            val=True,
+            plots=plots,
+            profile=False,
+        )
 
     def predict(self, frame):
         pass
