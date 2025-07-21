16188369e7bd4a9f2dadda05abdc017e4578ea0d
Sébastien Maes <sebastien.maes@protonmail.com>
2025-07-21 21:25:27 +0200

    pose.py: add member variables and not yet implemented methods

--- pose.py
+++ pose.py
@@ -13,16 +13,48 @@ logger = logging.getLogger(__name__)
 
 
 class Pose(Task):
+    """
+    Pose estimation task class for YOLO models.
+    """
+
     def __init__(
         self,
         model: Union[str, Path] = "yolo11s-pose.pt",
         confidence_threshold: float = 0.25,
+        verbose: bool = False,
     ) -> None:
-        self.model_path = model if model else "yolo11s-pose.pt"
-        self._model = YOLO(model=self.model_path, task="pose")
-        self._conf_threshold = confidence_threshold
+        self._verbose = verbose
+        self._name = "pose"
 
-    def predict(self, frame):
+        self.model_path = model if model else "yolo11s-pose.pt"
+        self._model = YOLO(
+            model=self.model_path,
+            task=self._name,
+            verbose=self._verbose,
+        )
+
+        # Sets the minimum confidence threshold for detections.
+        # Lower values increase recall but may introduce more false positives.
+        # Used during validation to compute precision-recall curves.
+        self._conf_threshold = confidence_threshold
+        # Threshold for Non-Maximum Supression
+        # Controls duplicate detection elimination
+        self._intersection_over_union = 0.7
+
+    def train(
+        self,
+        dataset="coco-pose.yaml",
+        img_size=640,
+        epochs=100,
+        device="cpu",
+        plots=False,
+    ):
+        pass
+
+    def predict(self, frame, half=False, device="cpu", visualize=False):
+        pass
+
+    def val(self, dataset="coco-pose.yaml", device="cpu"):
         pass
 
     def _annotate(
